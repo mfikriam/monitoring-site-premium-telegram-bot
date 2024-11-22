ad9b7dae95ea04f2d108d1c2982c31bffcbcb883
mfikriam <fikriaksan17@gmail.com>
2024-11-22 16:31:40 +0800

    Update datek

--- sites/Old/UPD104 OLD.js
+++ sites/Old/UPD104 OLD.js
@@ -1,9 +1,8 @@
 // SITE ID : UPD104
-// LINK 1  : ONT Fiberhome A5161
+// LINK 1  : ONT ZTE C300
 // LINK 2  : ONT ZTE C300
 
 import config from '../utils/config.js';
-import ONT_Fiberhome_A5161 from '../devices/ONT_Fiberhome_A5161.js';
 import ONT_ZTE_C300 from '../devices/ONT_ZTE_C300.js';
 import updateStatusCounter from '../utils/update-status-counter.js';
 
@@ -15,19 +14,18 @@ async function site({ index, msg, site, sshConfig, countStatusLink }) {
   telnetConfig = {
     username,
     password,
-    host: 'GPON07-D7-PNK-4',
-    command: ['config', 'show authorization 1/13/15 | include FHTT05e879f0'],
-    keyword: 'up',
+    host: 'GPON02-D7-PNK-3',
+    command: 'show gpon onu detail gpon-onu_1/4/13:50',
   };
-  console.log(`  > Link 1: ONT Fiberhome A5161`);
-  const statusLink1 = await ONT_Fiberhome_A5161({ sshConfig, telnetConfig });
+  console.log(`  > Link 1: ONT ZTE C300`);
+  const statusLink1 = await ONT_ZTE_C300({ sshConfig, telnetConfig });
 
   // LINK 2
   telnetConfig = {
     username,
     password,
-    host: 'GPON02-D7-PNK-3',
-    command: 'show gpon onu detail gpon-onu_1/4/13:50',
+    host: 'GPON01-D7-PNK-3',
+    command: 'show gpon onu detail gpon-onu_1/13/4:51',
   };
   console.log(`  > Link 2: ONT ZTE C300`);
   const statusLink2 = await ONT_ZTE_C300({ sshConfig, telnetConfig });
