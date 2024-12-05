372bf700c2c38f73e677a3143fe8820139e4d356
mfikriam <fikriaksan17@gmail.com>
2024-12-05 08:15:16 +0800

    Add Bat File Site IWIP & Premium

---
+++ Monitor Site IWIP.bat
@@ -0,0 +1,38 @@
+@echo off
+echo Pastikan Anda sudah terhubung ke Globalprotect!
+echo.
+echo Performing git fetch and reset to update the repository...
+git fetch origin
+if errorlevel 1 (
+    echo Git fetch failed. Please check your internet connection or repository URL.
+    pause
+    exit /b
+)
+git reset --hard origin/main
+if errorlevel 1 (
+    echo Git reset failed. Please check your repository or branch name.
+    pause
+    exit /b
+)
+echo Repository updated successfully.
+echo.
+
+echo Choose server location:
+echo [1] Sentul
+echo [2] Jatinegara
+set /p choice=Enter your choice (1 or 2): 
+
+if "%choice%"=="1" (
+    echo Running start:iwip:sentul...
+    echo.
+    npm run start:iwip:sentul
+) else if "%choice%"=="2" (
+    echo Running start:iwip:jatinegara...
+    echo.
+    npm run start:iwip:jatinegara
+) else (
+    echo Invalid choice. Exiting.
+    exit /b
+)
+
+pause
