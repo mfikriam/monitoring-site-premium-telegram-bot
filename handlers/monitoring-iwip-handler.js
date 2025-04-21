db92b93d65c5bb12d3de40be91cd5e17036e9dd5
mfikriam <fikriaksan17@gmail.com>
2025-04-21 15:52:50 +0800

    Create monitoring iwip backup

--- handlers/monitoring-iwip-handler.js
+++ handlers/monitoring-iwip-handler.js
@@ -1,101 +1,33 @@
-// IMPORT HANDLERS
-import excelHandler from './excel-handler.js';
-import multiPcdHandler from './multi-pcd-handler.js';
-
 // IMPORT UTILS
 import currentDateTime from '../utils/get-current-datetime.js';
-import getStatusDesc from '../utils/get-status-descriptions.js';
 
-async function getStatusLinks(linksObj, defaultConfig) {
-  const site = linksObj[0];
-  const nmsConfig = site.ne === 'METRO' ? { ...defaultConfig.metro.nms } : { ...defaultConfig.gpon.nms };
-  const neConfig = site.ne === 'METRO' ? { ...defaultConfig.metro.ne } : { ...defaultConfig.gpon.ne };
-
-  // PRINT LINK TITLE
-  console.log(`  > Link: ${site.ne}`);
-
-  // OVERWRITE NE CONFIG
-  if (site.username_ne && site.username_ne !== 'username_nms') {
-    neConfig.username = site.username_ne;
-    neConfig.password = site.password_ne;
-  }
-
-  await multiPcdHandler({ nmsConfig, neConfig, site, linksObj });
-
-  // console.log(linksObj);
-}
+const data = [
+  {
+    src: 'SFI',
+    dest: 'WDA',
+    interfaces: [],
+  },
+];
 
 async function monitoringPremiumHandler(msg, defaultConfig) {
-  // GENERATE INITIAL MESSAGE
-  msg += `<b>REPORT MONITORING SITE IWIP, MABA, & WEDA</b>\n`;
+  // Initial Message
+  msg += `<b>REPORT MONITORING CLUSTER IWIP</b>\n`;
   msg += `${currentDateTime()}\n`;
   msg += `\n`;
-  msg += `Site | IP NTE | Current BW (Max BW)\n`;
-  msg += `\n`;
 
-  // INITIALIZE VARIABLES
-  const allowedSites = [];
-  // allowedSites.push('ME-D7-IWP');
+  // Declare variables
+  let devicesRoute = [];
 
-  // GET DATA SITES
-  const iwipSites = await excelHandler('iwip-sites.xlsx');
-  if (iwipSites.length === 0) return null;
-
-  // FILTER SITES
-  const filteredSites =
-    allowedSites.length > 0 ? iwipSites.filter((site) => allowedSites.includes(site.site_id)) : iwipSites;
-
-  // GET SITE IDS
-  const uniqueSiteIds = [...new Set(filteredSites.map((item) => item.site_id))];
-
-  // MONITORING SITES
-  for (const [index, siteId] of uniqueSiteIds.entries()) {
-    // GET LINKS
-    const links = filteredSites.filter((site) => site.site_id === siteId);
-
-    // DEFINE SITE
-    const site = links[0];
-
-    // DEFINE LINK OBJECT
-    const linksObj = [];
-    links.forEach((link) => {
-      linksObj.push({ ...link, currentBW: 0, maxBW: 0, statusLink: '🟨' });
-    });
-
-    // PRINT SITE TITLE
-    console.log(`${index + 1}. ${site.site_id} - ${site.ne}`);
-
-    // GET STATUS LINKS
-    await getStatusLinks(linksObj, defaultConfig);
-
-    // CALCULATE THE SUM OF currentBW AND maxBW
-    const totals = linksObj.reduce(
-      (acc, item) => {
-        acc.currentBW += item.currentBW;
-        acc.maxBW += item.maxBW;
-        return acc;
-      },
-      { currentBW: 0, maxBW: 0 }, // Initial values
-    );
-    const currentBW = totals.currentBW === 0 ? '#' : `${totals.currentBW}G`;
-    const maxBW = totals.maxBW === 0 ? '#' : `${totals.maxBW}G`;
-
-    // ADD SUBDISTRCT TITLE TO MESSAGE
-    msg += `<b>${index + 1}. ${site.site_id} | ${site.ip_ne} | ${currentBW} (${maxBW})</b>\n`;
-
-    // ADD LINKS TO MESSAGE
-    for (const link of linksObj) {
-      const statusDesc = getStatusDesc(link.statusLink);
-      const currentBW = link.currentBW === 0 ? '#' : `${link.currentBW}G`;
-      const maxBW = link.maxBW === 0 ? '#' : `${link.maxBW}G`;
-      msg += `- ${link.site_id} → ${link.site_dest}, `;
-      msg += `${currentBW} (${maxBW}), `;
-      msg += `${link.interface_port_ne}, ${statusDesc} ${link.statusLink}\n`;
-    }
-
-    // ADD NEW LINE
-    msg += `\n`;
+  // ----------------- 1. Ring Metro-E via DWDM -----------------
+  devicesRoute = ['SFI', 'WDA', 'IWP', 'MBA', 'SFI'];
+  msg += `1. Ring Metro-E via DWDM\n`;
+  msg += `${devicesRoute[0]}`;
+  for (let i = 0; i < devicesRoute.length - 1; i++) {
+    const route1 = devicesRoute[i];
+    const route2 = devicesRoute[i];
+    msg += ` &lt;&gt; ${route2}`;
   }
+  msg += `\n`;
 
   return msg;
 }
