089aa39ecd9ddf58bd7c1f245cc862825920a6f0
mfikriam <fikriaksan17@gmail.com>
2025-05-05 09:48:48 +0800

    Add Single Port Device for L2SW FH S5800v2

--- handlers/monitoring-iwip-handler.js
+++ handlers/monitoring-iwip-handler.js
@@ -7,8 +7,9 @@ import currentDateTime from '../utils/get-current-datetime.js';
 // Import Devices
 import SPC_METRO from '../iwip-devices/SPC_METRO.js';
 import MPC_METRO from '../iwip-devices/MPC_METRO.js';
-import SPC_L2SW_FH_S5800_SERIES from '../iwip-devices/SPC_L2SW_FH_S5800_SERIES.js';
 import MPC_L2SW_FH_S5800_SERIES from '../iwip-devices/MPC_L2SW_FH_S5800_SERIES.js';
+import SPC_L2SW_FH_S5800v1 from '../iwip-devices/SPC_L2SW_FH_S5800v1.js';
+import SPC_L2SW_FH_S5800v2 from '../iwip-devices/SPC_L2SW_FH_S5800v2.js';
 import MPC_L2SW_RAISECOM from '../iwip-devices/MPC_L2SW_RAISECOM.js';
 
 async function deviceHandler(defaultConfig, datek, resObj) {
@@ -29,8 +30,10 @@ async function deviceHandler(defaultConfig, datek, resObj) {
       return await SPC_METRO(deviceParams);
     case 'MPC_METRO':
       return await MPC_METRO(deviceParams);
-    case 'SPC_L2SW_FH_S5800_SERIES':
-      return await SPC_L2SW_FH_S5800_SERIES(deviceParams);
+    case 'SPC_L2SW_FH_S5800v1':
+      return await SPC_L2SW_FH_S5800v1(deviceParams);
+    case 'SPC_L2SW_FH_S5800v2':
+      return await SPC_L2SW_FH_S5800v2(deviceParams);
     case 'MPC_L2SW_FH_S5800_SERIES':
       return await MPC_L2SW_FH_S5800_SERIES(deviceParams);
     case 'MPC_L2SW_RAISECOM':
@@ -56,123 +59,123 @@ async function monitoringPremiumHandler(msg, defaultConfig) {
 
   // ----------------------------- 1. Ring Metro-E via DWDM -----------------------------
 
-  // Print title
-  console.log(`[Ring Metro-E via DWDM]\n`);
+  // // Print title
+  // console.log(`[Ring Metro-E via DWDM]\n`);
 
-  // Define routes for Metro-E via DWDM
-  routes = ['SFI', 'WDA', 'IWP', 'MBA', 'SFI'];
+  // // Define routes for Metro-E via DWDM
+  // routes = ['SFI', 'WDA', 'IWP', 'MBA', 'SFI'];
 
-  // Define Interfaces NE for Metro-E via DWDM
-  interfacesNE = [
-    { src: 'SFI', dest: 'WDA', group_interface: 'Eth-Trunk25', ne: 'SPC_METRO' },
-    { src: 'WDA', dest: 'IWP', group_interface: 'Eth-Trunk11', ne: 'SPC_METRO' },
-    { src: 'IWP', dest: 'MBA', group_interface: 'Eth-Trunk25', ne: 'SPC_METRO' },
-    { src: 'MBA', dest: 'SFI', group_interface: 'Eth-Trunk23', ne: 'SPC_METRO' },
-  ];
+  // // Define Interfaces NE for Metro-E via DWDM
+  // interfacesNE = [
+  //   { src: 'SFI', dest: 'WDA', group_interface: 'Eth-Trunk25', ne: 'SPC_METRO' },
+  //   { src: 'WDA', dest: 'IWP', group_interface: 'Eth-Trunk11', ne: 'SPC_METRO' },
+  //   { src: 'IWP', dest: 'MBA', group_interface: 'Eth-Trunk25', ne: 'SPC_METRO' },
+  //   { src: 'MBA', dest: 'SFI', group_interface: 'Eth-Trunk23', ne: 'SPC_METRO' },
+  // ];
 
-  // Add title to message
-  msg += `1. Ring Metro-E via DWDM\n`;
-  msg += `${routes[0]}`;
+  // // Add title to message
+  // msg += `1. Ring Metro-E via DWDM\n`;
+  // msg += `${routes[0]}`;
 
-  // Loop through routes and update datek objects
-  for (let i = 0; i < routes.length - 1; i++) {
-    // Get source and destination
-    const src = routes[i];
-    const dest = routes[i + 1];
+  // // Loop through routes and update datek objects
+  // for (let i = 0; i < routes.length - 1; i++) {
+  //   // Get source and destination
+  //   const src = routes[i];
+  //   const dest = routes[i + 1];
 
-    // Print route title
-    console.log(`${i + 1}. ${src} → ${dest}`);
+  //   // Print route title
+  //   console.log(`${i + 1}. ${src} → ${dest}`);
 
-    // Find the datek object for the source
-    const datek = dateks.find((data) => data.id === src);
-    const datekDest = dateks.find((data) => data.id === dest);
+  //   // Find the datek object for the source
+  //   const datek = dateks.find((data) => data.id === src);
+  //   const datekDest = dateks.find((data) => data.id === dest);
 
-    // Get Datek NE
-    datek.ne = interfacesNE.find((route) => route.src === src && route.dest === dest).ne;
+  //   // Get Datek NE
+  //   datek.ne = interfacesNE.find((route) => route.src === src && route.dest === dest).ne;
 
-    // Get Datek Group Interface
-    datek.group_interface = interfacesNE.find((route) => route.src === src && route.dest === dest).group_interface;
+  //   // Get Datek Group Interface
+  //   datek.group_interface = interfacesNE.find((route) => route.src === src && route.dest === dest).group_interface;
 
-    // Initialize result object
-    const resObj = { currentBW: '#', maxBW: '#', statusLink: '🟨', interfaces: [] };
+  //   // Initialize result object
+  //   const resObj = { currentBW: '#', maxBW: '#', statusLink: '🟨', interfaces: [] };
 
-    // Call deviceHandler
-    datek.ne = 'SPC_METRO';
-    await deviceHandler(defaultConfig, datek, resObj);
+  //   // Call deviceHandler
+  //   datek.ne = 'SPC_METRO';
+  //   await deviceHandler(defaultConfig, datek, resObj);
 
-    // Check if any interfaces is down
-    if (resObj.statusLink === '❌') {
-      resObj.interfaces.forEach((data) => {
-        if (data.portStatus !== 'UP')
-          losInterfaces.push(`- ${datek.hostname_ne} ${data.portName} &lt;&gt; ${datekDest.hostname_ne} LOS ❌`);
-      });
-    }
+  //   // Check if any interfaces is down
+  //   if (resObj.statusLink === '❌') {
+  //     resObj.interfaces.forEach((data) => {
+  //       if (data.portStatus !== 'UP')
+  //         losInterfaces.push(`- ${datek.hostname_ne} ${data.portName} &lt;&gt; ${datekDest.hostname_ne} LOS ❌`);
+  //     });
+  //   }
 
-    // Add result object to message
-    msg += ` &lt;${resObj.currentBW}/${resObj.maxBW} ${resObj.statusLink}&gt; ${dest}`;
-  }
+  //   // Add result object to message
+  //   msg += ` &lt;${resObj.currentBW}/${resObj.maxBW} ${resObj.statusLink}&gt; ${dest}`;
+  // }
 
-  // Add LOS interfaces to message
-  if (losInterfaces.length > 0) {
-    msg += `\n\nDown :\n`;
-    losInterfaces.forEach((data) => {
-      msg += `${data}\n`;
-    });
-  }
+  // // Add LOS interfaces to message
+  // if (losInterfaces.length > 0) {
+  //   msg += `\n\nDown :\n`;
+  //   losInterfaces.forEach((data) => {
+  //     msg += `${data}\n`;
+  //   });
+  // }
 
-  // Add new line
-  msg += `\n`;
+  // // Add new line
+  // msg += `\n`;
 
   // --------------------------- End of 1. Ring Metro-E via DWDM ---------------------------
 
   // ----------------------------- 2. Ring Metro-E via Radio IP -----------------------------
 
-  // Print title
-  console.log();
-  console.log(`[Ring Metro-E via Radio IP]\n`);
+  // // Print title
+  // console.log();
+  // console.log(`[Ring Metro-E via Radio IP]\n`);
 
-  // Add title to message
-  msg += `\n`;
-  msg += `2. Ring Metro-E via Radio IP\n`;
+  // // Add title to message
+  // msg += `\n`;
+  // msg += `2. Ring Metro-E via Radio IP\n`;
 
-  // Get datek for WDA
-  const datek = dateks.find((data) => data.id === 'WDA');
-  const datekDest = dateks.find((data) => data.id === 'IWP');
+  // // Get datek for WDA
+  // const datek = dateks.find((data) => data.id === 'WDA');
+  // const datekDest = dateks.find((data) => data.id === 'IWP');
 
-  // Print route title
-  console.log(`1. WDA → IWP`);
+  // // Print route title
+  // console.log(`1. WDA → IWP`);
 
-  // Set interface NE
-  datek.interfaces_ne = ['Eth-Trunk1.10', 'Eth-Trunk1.11', 'Eth-Trunk1.12', 'Eth-Trunk1.13'];
+  // // Set interface NE
+  // datek.interfaces_ne = ['Eth-Trunk1.10', 'Eth-Trunk1.11', 'Eth-Trunk1.12', 'Eth-Trunk1.13'];
 
-  // Set Datek NE
-  datek.ne = 'MPC_METRO';
+  // // Set Datek NE
+  // datek.ne = 'MPC_METRO';
 
-  // Initialize result object
-  const resObj = {
-    numUpInterfaces: '#',
-    numInterfaces: '#',
-    statusLink: '🟨',
-    interfaces: datek.interfaces_ne.map((intf) => ({ portName: intf, portStatus: '#', resultString: '#' })),
-  };
+  // // Initialize result object
+  // const resObj = {
+  //   numUpInterfaces: '#',
+  //   numInterfaces: '#',
+  //   statusLink: '🟨',
+  //   interfaces: datek.interfaces_ne.map((intf) => ({ portName: intf, portStatus: '#', resultString: '#' })),
+  // };
 
-  // Call deviceHandler
-  await deviceHandler(defaultConfig, datek, resObj);
+  // // Call deviceHandler
+  // await deviceHandler(defaultConfig, datek, resObj);
 
-  // Add result object to message
-  msg += `WDA &lt;${resObj.numUpInterfaces}/${resObj.numInterfaces} ${resObj.statusLink}&gt; IWP`;
+  // // Add result object to message
+  // msg += `WDA &lt;${resObj.numUpInterfaces}/${resObj.numInterfaces} ${resObj.statusLink}&gt; IWP`;
 
-  // Check if any interfaces is down
-  if (resObj.statusLink === '❌') {
-    msg += `\n\nDown :\n`;
-    resObj.interfaces.forEach((data) => {
-      if (data.portStatus !== 'UP')
-        msg += `- ${datek.hostname_ne} ${data.portName} &lt;&gt; ${datekDest.hostname_ne} LOS ❌\n`;
-    });
-  }
+  // // Check if any interfaces is down
+  // if (resObj.statusLink === '❌') {
+  //   msg += `\n\nDown :\n`;
+  //   resObj.interfaces.forEach((data) => {
+  //     if (data.portStatus !== 'UP')
+  //       msg += `- ${datek.hostname_ne} ${data.portName} &lt;&gt; ${datekDest.hostname_ne} LOS ❌\n`;
+  //   });
+  // }
 
-  // Add new line
-  msg += `\n`;
+  // // Add new line
+  // msg += `\n`;
 
   // -------------------------- End of 2. Ring Metro-E via Radio IP --------------------------
 
@@ -184,26 +187,26 @@ async function monitoringPremiumHandler(msg, defaultConfig) {
 
   // Define routes for L2SW
   routes = [
-    'WDA',
-    'SSU020',
-    'IWP',
-    'SSU005',
-    'SSU043',
-    'OLD-SSU007',
-    'NEW-SSU007',
-    'SSU015',
+    // 'WDA',
+    // 'SSU020',
+    // 'IWP',
+    // 'SSU005',
+    // 'SSU043',
+    // 'OLD-SSU007',
+    // 'NEW-SSU007',
+    // 'SSU015',
     'MBA012',
     'MBA',
-    'BUL',
-    'SFI',
-    'SBM',
-    'MBA',
+    // 'BUL',
+    // 'SFI',
+    // 'SBM',
+    // 'MBA',
   ];
 
   // Define Interfaces NE for L2SW
   interfacesNE = [
     { src: 'WDA', dest: 'SSU020', group_interface: 'Eth-Trunk5', ne: 'SPC_METRO' },
-    { src: 'SSU020', dest: 'IWP', group_interface: 'eth-trunk 2', ne: 'SPC_L2SW_FH_S5800_SERIES' },
+    { src: 'SSU020', dest: 'IWP', group_interface: 'eth-trunk 2', ne: 'SPC_L2SW_FH_S5800v1' },
     { src: 'IWP', dest: 'SSU005', group_interface: 'Eth-Trunk10', ne: 'SPC_METRO' },
     { src: 'SSU005', dest: 'SSU043', interfaces_ne: ['10gigaethernet 1/0/16'], ne: 'MPC_L2SW_FH_S5800_SERIES' },
     { src: 'SSU043', dest: 'OLD-SSU007', interfaces_ne: ['xgigaethernet 1/1/1'], ne: 'MPC_L2SW_FH_S5800_SERIES' },
@@ -225,9 +228,9 @@ async function monitoringPremiumHandler(msg, defaultConfig) {
     {
       src: 'MBA012',
       dest: 'MBA',
-      interfaces_ne: ['xgigaethernet 1/0/39', 'xgigaethernet 1/0/40', 'xgigaethernet 1/0/41', 'xgigaethernet 1/0/42'],
-      ne: 'MPC_L2SW_FH_S5800_SERIES',
-      doublePagination: true,
+      group_interface: 'eth-trunk 1',
+      ne: 'SPC_L2SW_FH_S5800v2',
+      // doublePagination: true,
     },
     { src: 'MBA', dest: 'BUL', group_interface: 'Eth-Trunk9', ne: 'SPC_METRO' },
     {
---
+++ iwip-devices/SPC_L2SW_FH_S5800v2.js
@@ -0,0 +1,247 @@
+import { Client as SSHClient } from 'ssh2';
+
+function resultParser(resObj) {
+  // Initialize statusLink to UP
+  resObj.statusLink = '✅';
+
+  for (const intf of resObj.interfaces) {
+    // Get result string
+    const resultString = intf.resultString;
+
+    // Check keyword
+    const isUp =
+      (resultString.includes('admin state : up') && resultString.includes('current state : up')) ||
+      resultString.includes('Admin state is up,operation state is up');
+
+    // Update interface status
+    if (resultString && isUp) {
+      intf.portStatus = 'Up';
+    } else {
+      resObj.statusLink = '❌';
+      intf.portStatus = 'LOS';
+    }
+
+    // // Test LOS interface
+    // if (intf.portName === 'xgigaethernet 1/0/41') {
+    //   resObj.statusLink = '❌';
+    //   intf.portStatus = 'LOS';
+    // }
+
+    // Print status port
+    console.log(
+      `    - Status Interface ${intf.portName}: ${intf.portStatus} ${intf.portStatus === 'Up' ? '✅' : '❌'}`,
+    );
+  }
+
+  // Print status link
+  console.log(`    - Status Link: ${resObj.statusLink}`);
+}
+
+function bandwidthParser(resObj) {
+  // Extract Max BW & Current BW
+  const portBWMatch = resObj.bwString.match(/Cur-BW:\(M\):\s*(\d+)/);
+  const maxBWMatch = resObj.bwString.match(/Max-BW:\(M\):\s*(\d+)/);
+  resObj.currentBW = portBWMatch ? parseInt(portBWMatch[1], 10) / 1000 : 0; // Gbps
+  resObj.maxBW = maxBWMatch ? parseInt(maxBWMatch[1], 10) / 1000 : 0; // Gbps
+
+  // Extract Port List
+  const portListMatch = resObj.bwString.match(/Port-List:([a-z0-9]+)\s+([\d\/,]+)/i);
+  let portList = [];
+  if (portListMatch) {
+    const typeMap = {
+      xge: 'xgigaethernet',
+      '100ge': '100gigaethernet',
+      '40ge': '40gigaethernet',
+    };
+    const portType = typeMap[portListMatch[1].toLowerCase()] || portListMatch[1].toLowerCase();
+    const portNumbers = portListMatch[2].split(',').map((p) => p.trim());
+    portList = portNumbers.map((p) => `${portType} ${p}`);
+  }
+  resObj.interfaces = portList.map((intf) => ({ portName: intf, portStatus: '#', resultString: '#' }));
+}
+
+async function L2SW({ nmsConfig, neConfig, datek, resObj, timeout = 60000 }) {
+  return new Promise((resolve, reject) => {
+    // CREATE SSH CONN INSTANCE
+    const conn = new SSHClient();
+
+    // INITIALIZE TIMEOUT HANDLE
+    let timeoutHandle;
+
+    // ON READY
+    conn.on('ready', () => {
+      // PRINT CONNECTION TITLE
+      const connTitle = `ssh ${nmsConfig.username}@${nmsConfig.host} ${nmsConfig.password}`;
+      console.log(`    - SSH Connection Established: ${connTitle}`);
+
+      // TYPE & STREAM ON TERMINAL AFTER SSH
+      conn.shell((err, stream) => {
+        // ERROR HANDLING
+        if (err) {
+          console.log('    - Error Starting Shell');
+          return resolve();
+        }
+
+        // INITIALIZED VARIABLES
+        let result = '';
+        let linkResult = '';
+        let loggedin = false;
+        let firstCommand = false;
+        let commandExec = false;
+        let finished = false;
+        let streamClosed = false;
+        let currentCommand = '';
+        let indexLink = 0;
+        let indexPagination = 0;
+
+        // SET A TIMEOUT TO LIMIT STREAMING TIME
+        timeoutHandle = setTimeout(() => {
+          console.log('    - Streaming Timeout Exceeded');
+          stream.end(); // End the stream if timeout is reached
+          console.log('    - SSH Stream Closed');
+          conn.end(); // Close the SSH connection
+        }, timeout);
+
+        // STREAM CLOSE HANDLER
+        stream.on('close', () => {
+          clearTimeout(timeoutHandle); // Clear the timeout if stream closes before time limit
+          resultParser(resObj); // Parse the result when the stream closes
+          resolve();
+        });
+
+        // STREAM DATA HANDLER
+        stream.on('data', (data) => {
+          // CONVERT STREAM DATA TO STRING
+          const dataStr = data.toString();
+          // console.log(dataStr);
+
+          // STORE THE STREAM DATA
+          result += dataStr;
+          if (commandExec || firstCommand) linkResult += dataStr;
+
+          // Ensure RNO NMS SSH To NE is ready
+          if (!loggedin && dataStr.includes('rno7app:~$')) {
+            currentCommand = `telnet ${datek.ip_ne}`;
+            console.log(`    - Executing Command On RNO Server: ${currentCommand}`);
+            stream.write(`${currentCommand}\n`);
+          }
+
+          // HANDLE NE AUTH: USERNAME
+          if (dataStr.includes('Username:') && !loggedin) {
+            console.log(`    - Entering NE Username: ${neConfig.username}`);
+            stream.write(`${neConfig.username}\n`);
+          }
+
+          // HANDLE NE AUTH: PASSWORD
+          if (dataStr.includes('Password:') && !loggedin) {
+            loggedin = true;
+            console.log(`    - Entering NE Password: ${neConfig.password}`);
+            stream.write(`${neConfig.password}\n`);
+          }
+
+          // HANDLE NE AUTH FAILED
+          if (dataStr.includes('%No such user or bad password.')) {
+            console.log(`    - NE Auth Failed`);
+            conn.end();
+          }
+
+          // First Command
+          if (dataStr.includes(`${datek.hostname_ne}#`) && !firstCommand) {
+            firstCommand = true;
+            currentCommand = `show interface ${datek.group_interface} verbose`;
+            console.log(`    - Executing Command: ${currentCommand}`);
+            stream.write(`${currentCommand}\n`);
+          }
+
+          // Parsing Bandwith
+          if (dataStr.includes(`Port-List:`) && dataStr.includes(`${datek.hostname_ne}#`) && firstCommand) {
+            resObj.bwString = linkResult;
+            linkResult = '';
+            bandwidthParser(resObj);
+            console.log(`    - Parsing Max & Current Bandwith`);
+          }
+
+          // Start Check Port Status
+          if (
+            resObj.bwString &&
+            firstCommand &&
+            resObj.interfaces.length !== 0 &&
+            dataStr.includes(`${datek.hostname_ne}#`) &&
+            !commandExec &&
+            indexLink < resObj.interfaces.length
+          ) {
+            currentCommand = `show interface ${resObj.interfaces[indexLink].portName}`;
+            console.log(`    - Executing Command: ${currentCommand}`);
+            stream.write(`${currentCommand}\n`);
+            commandExec = true;
+          }
+
+          // Handle Paginations
+          if (dataStr.includes('--More--') && commandExec) {
+            result += '\n';
+
+            if (indexPagination === 0) {
+              resObj.interfaces[indexLink].resultString = linkResult;
+              console.log(`    - Save Result String for ${resObj.interfaces[indexLink].portName}`);
+            } else {
+              linkResult = '';
+            }
+
+            indexPagination++;
+            console.log('    - Pagination Detected: Sending Space');
+            stream.write(' ');
+          }
+
+          // Move To Next Interface
+          if (dataStr.includes(`Other statistic:`) && dataStr.includes(`${datek.hostname_ne}#`) && commandExec) {
+            console.log('    - Move to next interface');
+            linkResult = '';
+            indexPagination = 0;
+            indexLink++;
+
+            if (indexLink < resObj.interfaces.length) {
+              currentCommand = `show interface ${resObj.interfaces[indexLink].portName}`;
+              stream.write(`${currentCommand}\n`);
+              console.log(`    - Executing Command: ${currentCommand}`);
+            }
+          }
+
+          // HANDLE FINISHING
+          if (
+            resObj.bwString &&
+            firstCommand &&
+            dataStr.includes(`${datek.hostname_ne}#`) &&
+            !finished &&
+            indexLink === resObj.interfaces.length
+          ) {
+            finished = true;
+          }
+
+          // HANDLE CLOSING SSH CONNECTION
+          if (resObj.bwString && finished && !streamClosed) {
+            streamClosed = true;
+            console.log('    - Closing SSH Connection');
+            conn.end();
+          }
+        });
+
+        // TELNET TO NE VIA IP ADDRESS
+        console.log(`    - Executing Command: telnet ${datek.ip_ne}`);
+        stream.write(`telnet ${datek.ip_ne}\n`);
+      });
+    });
+
+    // ON ERROR
+    conn.on('error', (err) => {
+      clearTimeout(timeoutHandle); // Clear the timeout on error
+      console.log('    - SSH Connection Error (SSH Failed)');
+      console.log(err);
+      resolve();
+    });
+
+    // ON CONNECT
+    conn.connect(nmsConfig);
+  });
+}
+
+export default L2SW;
