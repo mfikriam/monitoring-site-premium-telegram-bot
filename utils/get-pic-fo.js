c6aedf18527a347768908bf42a80a19068bcd5a6
mfikriam <fikriaksan17@gmail.com>
2025-05-09 08:42:19 +0800

    Update PIC FO

--- utils/get-pic-fo.js
+++ utils/get-pic-fo.js
@@ -1,11 +1,11 @@
 function getPICFO(witels) {
   const picFO = {
     SULSEL: '@jodryjft @BOSSNYA_PENGUSAHA @jeffrianto87 @ommaros',
-    'SULSEL BARAT': '@markus_taba @SUTRYSNO_HADISAPUTRA @Lannyadelin',
+    'SULSEL BARAT': '@anthyarifin @SUTRYSNO_HADISAPUTRA @Lannyadelin',
     SULTRA: '@hermanbatari @President_Mr @rizaldy_pahlevi @Adityaerawann',
     SULTENG: '@flow_like_river @Cloverdifferents @indralaopa',
     GORONTALO: '@GoGtoOlo @Internet_Positiff @imam_alr @mpragnya',
-    'SULUT MALUT': '@ndrew_MP @Putu_Oka_082187385141 @ismailahsan',
+    'SULUT MALUT': '@JohannNicky @Putu_Oka_082187385141 @mirino_aem',
     MALUKU: '@ABO_AO_JOHN @Ampongjua @noviyntk',
     PAPUA: '@Syowii @cumaAnakBawang @fazanugroho',
     'PAPUA BARAT': '@engelberthus @Selangkah_Di_Depan @Pitgbay',
