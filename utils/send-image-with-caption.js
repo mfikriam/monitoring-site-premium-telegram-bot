9e9a8115601384a252d9c148b97be0e0a63b52e1
mfikriam <fikriaksan17@gmail.com>
2025-06-02 13:36:54 +0800

    Add redaction when result LOS to Reroute Group

--- utils/send-image-with-caption.js
+++ utils/send-image-with-caption.js
@@ -4,7 +4,14 @@ import sanitizeMessage from './sanitize-message.js';
 
 async function sendImageWithCaption(bot, chatId, resMsg) {
   const { caption, imageBuffer } = resMsg;
-  const sanitizedCaption = sanitizeMessage(caption); // Sanitize the text/caption
+  let modifiedCaption = caption; // Make caption mutable
+
+  // Add redaction if the msg sent to group REROUTE VLAN TREG 7
+  if (chatId === '-4674089782' && modifiedCaption.includes('❌')) {
+    modifiedCaption += `\n\nMohon untuk dilakukan pengecekan dan diopenkan tiket preventive jika perlu @AndiFebriani @IOC7_QUALITY_0813483933 @SURVEILLANCE_TIF4_MSO7`;
+  }
+
+  const sanitizedCaption = sanitizeMessage(modifiedCaption); // Sanitize the text/caption
   const messages = splitMessage(sanitizedCaption, 1024); // Telegram's max message length
 
   // Send the first part as the caption with the image
--- utils/send-text-message.js
+++ utils/send-text-message.js
@@ -3,10 +3,16 @@ import splitMessage from './split-message.js';
 import sanitizeMessage from './sanitize-message.js';
 
 async function sendTextMessage(bot, chatId, text) {
-  const sanitizedText = sanitizeMessage(text); // Sanitize the text
+  let modifiedText = text; // Make text mutable
+
+  // Add redaction if the msg sent to group REROUTE VLAN TREG 7
+  if (chatId === '-4674089782' && modifiedText.includes('❌')) {
+    modifiedText += `\n\nMohon untuk dilakukan pengecekan dan diopenkan tiket preventive jika perlu @AndiFebriani @IOC7_QUALITY_0813483933 @SURVEILLANCE_TIF4_MSO7`;
+  }
+
+  const sanitizedText = sanitizeMessage(modifiedText); // Sanitize the text
   const messages = splitMessage(sanitizedText, 4096); // Telegram's max message length is 4096
 
-  // Use for...of loop to await message sending to ensure order
   for (const message of messages) {
     await bot.sendMessage(chatId, message, { parse_mode: 'HTML' });
   }
